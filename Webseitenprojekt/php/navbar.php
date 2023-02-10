b054fbd1b2536bd941ec1a93f0ba89b1c7a056c9
Cur1o <2021gmehersan@gmail.com>
2023-02-11 00:35:04 +0100

    Update navbar.php

--- Webseitenprojekt/php/navbar.php
+++ Webseitenprojekt/php/navbar.php
@@ -13,6 +13,12 @@
 			<a href="index.php?action=login" >Login</a>
 			<a href="index.php?action=register">Register</a>
 			';
+		}else{
+			echo'
+			<a href="index.php?action=Shop1">Shop 1</a>
+			<a href="index.php?action=Shop2">Shop 2</a>
+			<a href="index.php?action=Shop3">Shop 3</a>
+			';
 		}?>
 	</div>
 </nav>
