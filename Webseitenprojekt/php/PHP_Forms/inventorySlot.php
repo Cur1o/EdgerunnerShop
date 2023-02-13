24b73b9cf2e3aebc0be5a9d852f982e04b681f1e
Cur1o <2021gmehersan@gmail.com>
2023-02-14 00:26:24 +0100

    Update inventorySlot.php

--- Webseitenprojekt/php/PHP_Forms/inventorySlot.php
+++ Webseitenprojekt/php/PHP_Forms/inventorySlot.php
@@ -1,30 +1,30 @@
 <form class="inventorySlot">
     <?php
         function getItem($itemID){
-            $conn = dbConnect();
-            try{
-                $query = $conn->prepare('SELECT products.name, 
-                products.description, products.image, 
-                products.price, products.isConsumeable
-                FROM products
-                WHERE products.id = ?');
-                $query->bindParam( 1, $itemID, PDO::PARAM_INT );
-                $query->execute();
-                if($data = $query->fetchAll(PDO::FETCH_ASSOC))
-                {   
-                    // $productName = $data[0]['name'];
-                    // $productDescription = $data['description']; 
-                    // //$productImage = $data['image'];
-                    // $productPrice = $data['price'];
-                    // $productIsConsumeable = $data['isConsumeable'];
-                    // echo($productName.' '.$productDescription.' '.$productPrice);
-                }
-            }catch(Exception $e){
-                userMessage('Es ist Fehler aufgetreten'.$e->getMessage());
-                $conn=null;
-                return false;   
-            }
+            // $conn = dbConnect();
+            // try{
+            //     $query = $conn->prepare('SELECT products.name, 
+            //     products.description, products.image, 
+            //     products.price, products.isConsumeable
+            //     FROM products
+            //     WHERE products.id = ?');
+            //     $query->bindParam( 1, $itemID, PDO::PARAM_INT );
+            //     $query->execute();
+            //     if($data = $query->fetchAll(PDO::FETCH_ASSOC))
+            //     {   
+            //         // $productName = $data[0]['name'];
+            //         // $productDescription = $data['description']; 
+            //         // //$productImage = $data['image'];
+            //         // $productPrice = $data['price'];
+            //         // $productIsConsumeable = $data['isConsumeable'];
+            //         // echo($productName.' '.$productDescription.' '.$productPrice);
+            //     }
+            // }catch(Exception $e){
+            //     userMessage('Es ist Fehler aufgetreten'.$e->getMessage());
+            //     $conn=null;
+            //     return false;   
+            // }
         }
     ?>
-    <!-- <input type="image" src="ProductImages/'..'" alt="500"/> -->
+    <input type="image" src="ProductImages/'..'" alt="500"/>
 </form>
