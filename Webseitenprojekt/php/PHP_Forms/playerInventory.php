591530fc7d8a6c0adbfd0dcf3524b02699de2333
Cur1o <103188701+Cur1o@users.noreply.github.com>
2023-02-16 15:13:02 +0100

    update smal change

--- Webseitenprojekt/php/PHP_Forms/playerInventory.php
+++ Webseitenprojekt/php/PHP_Forms/playerInventory.php
@@ -25,7 +25,8 @@
                 } 
                 echo '</div>';
             }else{
-                echo('No inventory slot');
+                
+
             }
             $conn = null;
         }catch(Exception $e){
@@ -55,7 +56,7 @@
                 } 
                 echo '</div>';
             }else{
-                echo('No inventory slot');
+                //TODO:
             }
             $conn = null;
         }catch(Exception $e){
@@ -66,20 +67,12 @@
     }
     //Buy an item from the shop
     function buyItem($slotID) {
-        echo'TESTING buy item: '.$slotID,$_SESSION['currentshopID'];
         $conn = dbConnect();
         try{
             $query = $conn->prepare('UPDATE user_resources SET userId = ? WHERE user_resources.id = ?');
             $query->bindParam( 1, $_SESSION['id'], PDO::PARAM_INT );
             $query->bindParam( 2, $slotID, PDO::PARAM_INT );
             $query->execute();
-            
-            $_SESSION['buyItemSucess'] = true;
-
-
-
-            // if($data = $query->fetchAll(PDO::FETCH_ASSOC)){
-            // }
         }catch(Exception $e){
             userMessage('Es ist Fehler aufgetreten'.$e->getMessage());
             $conn=null;
@@ -89,15 +82,12 @@
     }
     //sell an item to the shop
     function sellItem($slotID) {
-        echo'TESTING sell item :'.$slotID,$_SESSION['currentshopID'];
         $conn = dbConnect();
         try{
             $query = $conn->prepare('UPDATE user_resources SET userId = ? WHERE id = ? ');
             $query->bindParam( 1, $_SESSION['currentshopID'], PDO::PARAM_INT );
             $query->bindParam( 2, $slotID, PDO::PARAM_INT );
             $query->execute();
-            // if($data = $query->fetchAll(PDO::FETCH_ASSOC)){
-            // }
         }catch(Exception $e){
             echo 'FEHLER';
             userMessage('Es ist Fehler aufgetreten'.$e->getMessage());
@@ -106,15 +96,28 @@
         }
         $conn = null;
     }
+
+    function sortErrorInventory(){
+        switch ($_SESSION['currentshopID']) {
+            case '1':
+                echo 'Der scho hat gerade nichts im angebot';    
+                break;
+            case '2':
+                echo 'Der scho hat gerade nichts im angebot';
+                break;
+            case '3':
+                echo 'Der scho hat gerade nichts im angebot';
+                break;
+            default:
+               
+                break;
+        }
+    }
           
     //end of PHP -------------------------------------------------------------------------------------------------------------------------------
 ?>
 <section class="playerInventoryFrame">
         <?php 
-            
-            
-           
-            
             // Überprüft ob daten von den items die verkauft oder gekauft wurden exestieren uns speichet diese zur verwendung
             if(isset($_POST['slotID'])&& isset($_POST['isShop'])){
                 $slotID = $_POST['slotID'];
@@ -122,48 +125,28 @@
                     sellItem($slotID);// wenn die gesendete slot id NICHT von einem shop kommt
                 else
                     buyItem($slotID);// wenn die gesendete slot id von einem shop kommt
-
-                //GetShopInventory($_SESSION['currentshopID']);
             }
 
-
             getItemsInInventory();
+            if($_GET['action'] == 'noShop'){
+                $shopIDGlobal = -1; 
+                $_SESSION['currentshopID'] = $shopIDGlobal;
 
-
-
+            } 
             if($_GET['action'] == 'Shop1'){
                 $shopIDGlobal = 1; 
                 $_SESSION['currentshopID'] = $shopIDGlobal;
-                
-                
-            }
-                
+
+            }    
             if($_GET['action'] == 'Shop2'){
                 $shopIDGlobal = 2;
                 $_SESSION['currentshopID'] = $shopIDGlobal;
-              
-                
             }
                 
             if($_GET['action'] == 'Shop3'){
                 $shopIDGlobal = 3;
                 $_SESSION['currentshopID'] = $shopIDGlobal;
-           
-               
             }
             GetShopInventory($_SESSION['currentshopID']);
         ?>
 </section>
-
-<!-- waffen verkaufen sql 
-(UPDATE user_resources SET userId = ? WHERE user_resources.id = ?) 
-1 = shop user id 
-2 = item slot id  
-
-
-waffen loeschen 
-"DELETE FROM user_resources WHERE `user_resources`.`userid` = ?"
-1 = shopuser
-
-
--->
