a40d2f1213f567dfd5aca4db7da5d4bae55891b5
Cur1o <103188701+Cur1o@users.noreply.github.com>
2023-02-20 16:00:34 +0100

    comentary finish

--- Itemarket_Herberich_Laufer_/web/index.php
+++ Itemarket_Herberich_Laufer_/web/index.php
@@ -1,27 +1,27 @@
 <?php
-  require_once('PHP/init.php');
-  require_once('PHP/account.php');
-
-  if(isset($_POST['button500'])) AddUserCoin(500);
-  if(isset($_POST['button1000'])) AddUserCoin(1000);
-  if(isset($_POST['button2000'])) AddUserCoin(2000);
-  if(isset($_POST['button4000'])) AddUserCoin(4000);
-  if(isset($_POST['button10000'])) AddUserCoin(10000);
+  require_once('PHP/init.php');     // Initialisierung der Anwendung
+  require_once('PHP/account.php');  // Einbindung der Account-Funktionalitäten aus account.php
+  //Die folgenden methodendefinition befindet sich in accaunt.php.
+  if(isset($_POST['button500'])) AddUserCoin(500);      //Dem Spieler werden nach druck auf den Button in coinsWindow.php 500 Coins hinzugefügt.
+  if(isset($_POST['button1000'])) AddUserCoin(1000);    //Dem Spieler werden nach druck auf den Button in coinsWindow.php 1000 Coins hinzugefügt.
+  if(isset($_POST['button2000'])) AddUserCoin(2000);    //Dem Spieler werden nach druck auf den Button in coinsWindow.php 2000 Coins hinzugefügt.
+  if(isset($_POST['button4000'])) AddUserCoin(4000);    //Dem Spieler werden nach druck auf den Button in coinsWindow.php 4000 Coins hinzugefügt.
+  if(isset($_POST['button10000'])) AddUserCoin(10000);  //Dem Spieler werden nach druck auf den Button in coinsWindow.php 10000 Coins hinzugefügt.
 
   //Frontend actions
-  //register
-  if(isset($_POST['enter']) && $_POST['enter']== 'Register')
-    if(isValidNick($_POST['nick']) && isValidEmail($_POST['email']))
-      register(strip_tags($_POST['nick']), strip_tags($_POST['email']), strip_tags($_POST['password']));
+  //regestrieren
+  if(isset($_POST['enter']) && $_POST['enter']== 'Register')//wenn enter gesetzt wurde und der übergebene wert
+    if(isValidNick($_POST['nick']) && isValidEmail($_POST['email']))  //Wenn der niick und die email noch nicht verwendet wurde.
+      register(strip_tags($_POST['nick']), strip_tags($_POST['email']), strip_tags($_POST['password'])); // Diese Funktion entfernt alle HTML- und PHP-Tags aus dem Parameter, um XSS-Angriffe zu verhindern.
 
-  //login
-  if(isset($_POST['enter']) && $_POST['enter'] == 'Login')
-    if(isValidNick( $_POST['nick']))
-      login( strip_tags($_POST['nick']), strip_tags($_POST['password']) );
+  //einloggen
+  if(isset($_POST['enter']) && $_POST['enter'] == 'Login')  //wenn enter gesetzt wurde und der übergebene wert
+    if(isValidNick( $_POST['nick']))  //Wenn der nick richtig ist                      
+      login( strip_tags($_POST['nick']), strip_tags($_POST['password']) );//  Diese Funktion entfernt alle HTML- und PHP-Tags aus dem Parameter, um XSS-Angriffe zu verhindern.
 
-  //logout
-  if( isset($_GET['action'] ) && $_GET['action'] == "logout" )
-    logout();
+  //ausloggen
+  if( isset($_GET['action'] ) && $_GET['action'] == "logout" )  //Wenn der logout button gedrückt wurde
+    logout(); //Methode Logout wird aufgerufen in accaunt.php
 
 ?>
 
@@ -34,10 +34,8 @@
     
     <!-- Einbindung des Style -->
     <link rel="stylesheet" href="style.CSS">
- </head>
-
+</head>
  <body>
-    
     <?php if($_SESSION['currentshopID'] == -1)
           include_once 'PHP/navbar.php';  //im normalen gebrauch wird die nav bar zuerst geladen
      ?>
@@ -64,21 +62,20 @@
           //falls später mal ein Admin besondere rechte haben soll
         }
       }
-      else if(isset($_GET['action'])){  //wenn kein benutzer Angemeldet ist oder er sich ausloggt
-        if($_GET['action'] == "register") //Wenn der regestrierungsknopf in navbar gedrückt wird
-          include 'PHP/PHP_Forms/registerWindow.php';
-        else if( $_GET['action'] == 'login')
-          include 'PHP/PHP_Forms/loginWindow.php';
+      else if(isset($_GET['action'])){                //wenn kein benutzer Angemeldet ist oder er sich ausloggt
+        if($_GET['action'] == "register")             //Wenn der regestrierungsknopf in navbar gedrückt wird.
+          include 'PHP/PHP_Forms/registerWindow.php'; //Es wird die form registerWindow.php eingebunden.
+        else if( $_GET['action'] == 'login')          //Wenn der login knopf in navbar gedrückt wird.
+          include 'PHP/PHP_Forms/loginWindow.php';    //Es wird die form loginWindow.php eingebunden.
       }
     ?>
     <!-- Die Nav Bar wird in die seite eingebunden -->
     <?php include 'PHP/navbar.php'; ?>  
-
   </body>
-  <script>
-    let nickName = "<?php if(isset($_SESSION['nick'])){echo $_SESSION['nick'];}?>";
-    if(nick != null && nick !="")
-      document.getElementById("nick").innerText = "Angemeldet als "+ nickName;
-  </script>
+    <script>
+      let nickName = "<?php if(isset($_SESSION['nick'])){echo $_SESSION['nick'];}?>";
+      if(nick != null && nick !="")
+        document.getElementById("nick").innerText = "Angemeldet als "+ nickName;
+    </script>
 
 </html>
