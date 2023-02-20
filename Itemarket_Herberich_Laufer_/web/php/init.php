4c8568b5917725138764a5e6872953e2e23fcf95
Cur1o <103188701+Cur1o@users.noreply.github.com>
2023-02-20 14:17:28 +0100

    comentary

--- Itemarket_Herberich_Laufer_/web/php/init.php
+++ Itemarket_Herberich_Laufer_/web/php/init.php
@@ -1,36 +1,36 @@
 <?php
-	session_start();
-	error_reporting(0);
+	session_start();	//Session starten
+	error_reporting(0);	//Fehlerausgabe blockieren 
 
 	function dbConnect(){
-		$host = 'localhost';	//der aktuelle host
-		$dbname ='db_cybershop';
-		$user = 'root';
-		$pass = '';
+		$host = 'localhost';		//der aktuelle host der auf die Datenbank zugreifen will
+		$dbname ='db_cybershop';	//Der name der Datenbank
+		$user = 'root';				//Der nutzer der auf die datanban zu greift
+		$pass = '';					
 		
 		try{
-			$DB = new PDO("mysql:host=".$host."; dbname=".$dbname.";", $user, $pass);
-			$DB->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-			return $DB;
-		}catch( PDOException $error){
-			die("Sorry ein Fehler ist aufgetreten, prüfen Sie Ihre Internetverbindung.Error:".$error);
+			$DB = new PDO("mysql:host=".$host."; dbname=".$dbname.";", $user, $pass);	//Datenbankverbindung wird aufgebaut
+			$DB->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );				//Atrebute zur fehlerausgabe werden gesetzt.
+			return $DB;					//Die Datenbank wird zuückgegeben.
+		}catch( PDOException $error){	//Wenn die Datenbankverbindung nicht funktioniert.
+			die("Sorry ein Fehler ist aufgetreten, prüfen Sie Ihre Internetverbindung.Error:".$error);	//fehlerausgabe an den nutzer 
 		}
 	}
 
-	function isAuthorized(){
+	function isAuthorized(){	//ob den nutzer autoresiert ist sich anzumelden.
 		return (isset($_SESSION['nick']) && isset($_SESSION['access']) && ($_SESSION['access'] != 'locked'));
 	}
 
-	function isAuthorizedAsAdmin(){
+	function isAuthorizedAsAdmin(){	//ob der Nutzer als Admin angemeldet ist.
 		return (isset($_SESSION['nick']) && isset($_SESSION['access']) && ($_SESSION['access'] == 'admin'));
 	}
 
 	//User messages------------------------------------------------------------------------------
 
-	if(!isset($_SESSION['userMessage']))
-		$_SESSION['userMessage']="";
+	if(!isset($_SESSION['userMessage']))//Wenn in user mesage ein wert gesetzt wurde wird dieser mit einem leeren string ersetzt
+		$_SESSION['userMessage']="";	//Session Variable wird dur einen leeren string ersetzt.
 
-	function userMessage( $message ){
-		$_SESSION['userMessage'] .= "$message<br>";
+	function userMessage( $message ){				//Die function wird oft aufgerufen um dem nutzer einen fehler auszugeben.
+		$_SESSION['userMessage'] .= "$message<br>";	//nach der geschriebenen Nachricht wird ein zeilenumbruch geschrieben
 	}
 ?>
