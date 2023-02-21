9bd3eebdd17552e4824158f374b66a0e17352a44
Cur1o <103188701+Cur1o@users.noreply.github.com>
2023-02-21 12:03:44 +0100

    update

---
+++ Webside_Freidel_Herberich_Laufer/web/php/navbar.php
@@ -0,0 +1,30 @@
+<nav>
+	<?php 
+		$coinsvalue = $_SESSION['EdgeCoins'];
+	?>
+	<div class="HomepageLink">
+		<!-- Hier wird die action noShop in playerInventory.php aufgerufen -->
+		<a href="index.php?action=noShop" id="HomepageName">Edgerunner Market</a>	
+	</div>
+	
+	<div>
+		<!-- Hier werden die aktuellen Coins angezeigt die action coins führt zu  -->
+		<a href="index.php?action=coins" ><?php  echo($coinsvalue); ?></a>
+		
+		<?php if(!isset($_SESSION['nick'])) {
+			// Wenn der spieler noch nicht angemeldet ist wird ihm hier die möglichkeit gegeben sich 
+			//anzumelden oder zu regestrieren die Actions führen zu index.php
+			echo '
+			<a href="index.php?action=login" >Login</a>
+			<a href="index.php?action=register">Register</a>
+			';
+		}else{
+			// Hier wird die action für Shop 1,2,3 in playerInventory.php aufgerufen 
+			echo'
+			<a href="index.php?action=Shop1">Waffen</a>
+			<a href="index.php?action=Shop2">Schutz</a>
+			<a href="index.php?action=Shop3">Sonstiges</a>
+			';
+		}?>
+	</div>
+</nav>
